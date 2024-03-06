04688873b830ec83867eade5de21ad84ffe1daff
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-03-06 20:31:15 -0300

    organized frontend

--- infinity/frontend/src/App.js
+++ infinity/frontend/src/App.js
@@ -1,33 +1,9 @@
-import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from "./Home";
 import AnotherScreen from './AnotherScreen';
 import './App.css';
 
 const App = () => {
-    const [toPrint, setToPrint] = useState('');
-    const [response, setResponse] = useState('');
-
-    const handlePrint = async () => {
-        try {
-            const response = await fetch('http://localhost:8080/api/v1/infinity/print', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'text/plain',
-                },
-                body: toPrint,
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
-            const result = await response.text();
-            setResponse(result);
-        } catch (error) {
-            console.error('Error:', error.message);
-        }
-    };
-
     return (
         <Router>
             <div>
@@ -43,25 +19,12 @@ const App = () => {
                 </nav>
 
                 <Routes>
-                    <Route path="/red" element={<AnotherScreen />} />
+                    <Route
+                        path="/red"
+                        element={<AnotherScreen />} />
                     <Route
                         path="/"
-                        element={
-                            <div>
-                                <h1>Spring Boot Kotlin Frontend</h1>
-                                <textarea
-                                    placeholder="Enter text to print"
-                                    value={toPrint}
-                                    onChange={(e) => setToPrint(e.target.value)}
-                                />
-                                <button className="button1" onClick={handlePrint}>
-                                    Print Something
-                                </button>
-                                <div>
-                                    <strong>Response:</strong> {response}
-                                </div>
-                            </div>
-                        }
+                        element={<Home />}
                     />
                 </Routes>
             </div>
---
+++ infinity/frontend/src/Home.js
@@ -0,0 +1,49 @@
+import React, {useState} from "react";
+
+const Home = () => {
+    const [toPrint, setToPrint] = useState('');
+    const [response, setResponse] = useState('');
+
+    const handlePrint = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/api/v1/infinity/print', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'text/plain',
+                },
+                body: toPrint,
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const result = await response.text();
+            setResponse(result);
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Spring Boot Kotlin Frontend</h1>
+            <textarea
+                placeholder="Enter text to print"
+                value={toPrint}
+                onChange={(e) => setToPrint(e.target.value)}
+            />
+            <button className="button1" onClick={handlePrint}>
+                Print Something
+            </button>
+            <div>
+                <strong>Response:</strong> {response}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
+
+
+
