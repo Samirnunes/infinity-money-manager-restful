dacc65662cff49e18f7ca230e5e6a9437833fc8a
joao <joaolucasrrolim@gmail.com>
2024-04-07 13:50:20 -0300

    Finish composicao filters

--- infinity/src/main/kotlin/com/ita/infinity/controllers/api/ComposicaoController.kt
+++ infinity/src/main/kotlin/com/ita/infinity/controllers/api/ComposicaoController.kt
@@ -5,24 +5,33 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/v1/infinity/composicao")
 @CrossOrigin(origins = ["http://localhost:3000"])
 class ComposicaoController {
+    class GetGastosUnicosByFilterBody {
+        public var categorias: ArrayList<String> = ArrayList()
+    }
+
     @Autowired
     private lateinit var composicaoScreen: ComposicaoScreen
 
     @GetMapping("get-gastos-unicos")
     fun getGastosUnicosByFilter(
         @RequestParam periodo: Long = 30,
-        @RequestParam categorias: ArrayList<String> = ArrayList()
+        @RequestBody getGastosUnicosByFilterBody: GetGastosUnicosByFilterBody
     ) : MutableList<MutableMap<String, Any>>? {
-        return composicaoScreen.getGastosUnicosByFilter(periodo, categorias)
+        return composicaoScreen.getGastosUnicosByFilter(periodo, getGastosUnicosByFilterBody.categorias)
+    }
+
+    class GetGanhosUnicosByFilterBody {
+        public var categorias: ArrayList<String> = ArrayList()
     }
 
     @GetMapping("get-ganhos-unicos")
     fun getGanhosUnicosByFilter(
         @RequestParam periodo: Long = 30,
-        @RequestParam categorias: ArrayList<String> = ArrayList()
+        @RequestParam getGanhosUnicosByFilterBody: GetGanhosUnicosByFilterBody
     ) : MutableList<MutableMap<String, Any>>? {
-        return composicaoScreen.getGanhosUnicosByFilter(periodo, categorias)
+        return composicaoScreen.getGanhosUnicosByFilter(periodo, getGanhosUnicosByFilterBody.categorias)
     }
 }
--- infinity/src/main/kotlin/com/ita/infinity/services/ComposicaoScreen.kt
+++ infinity/src/main/kotlin/com/ita/infinity/services/ComposicaoScreen.kt
@@ -4,8 +4,6 @@ import com.example.infinitymoneymanager.databaseClasses.GastoUnico
 import com.example.infinitymoneymanager.databaseClasses.GanhoUnico
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.RequestParam
-import java.time.LocalDate
 
 
 @Service
@@ -14,24 +12,25 @@ class ComposicaoScreen {
     private lateinit var dbConnector: DatabaseConnector
 
     fun getGastosUnicosByFilter(
-        @RequestParam periodo: Long = 30,
-        @RequestParam categorias: ArrayList<String> = ArrayList()
+        periodo: Long = 30,
+        categorias: ArrayList<String>,
     ) : MutableList<MutableMap<String, Any>>? {
-        val periodoCondition: String = "data < (NOW() - INTERVAL $periodo DAY)"
-        val categoriasSet = categorias.joinToString(prefix = "(", separator = ", ", postfix = ")")
+        val periodoCondition: String = "data > (NOW() - INTERVAL $periodo DAY)"
+        val categoriasSet = categorias.joinToString(prefix = "('", separator = "', '", postfix = "')")
         val categoriasCondition = "categoria NOT IN $categoriasSet"
         val whereCondition = "$periodoCondition AND $categoriasCondition"
+        println(whereCondition)
         dbConnector.use {
             return it.select(GastoUnico(), whereCondition = whereCondition)
         }
     }
 
     fun getGanhosUnicosByFilter(
-        @RequestParam periodo: Long = 30,
-        @RequestParam categorias: ArrayList<String> = ArrayList()
+        periodo: Long = 30,
+        categorias: ArrayList<String>,
     ) : MutableList<MutableMap<String, Any>>? {
-        val periodoCondition: String = "data < (NOW() - INTERVAL $periodo DAY)"
-        val categoriasSet = categorias.joinToString(prefix = "(", separator = ", ", postfix = ")")
+        val periodoCondition: String = "data > (NOW() - INTERVAL $periodo DAY)"
+        val categoriasSet = categorias.joinToString(prefix = "('", separator = "', '", postfix = "')")
         val categoriasCondition = "categoria NOT IN $categoriasSet"
         val whereCondition = "$periodoCondition AND $categoriasCondition"
         dbConnector.use {
